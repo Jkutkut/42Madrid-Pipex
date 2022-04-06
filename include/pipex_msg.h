d85272fb19badd3cd5490f58c50d0212825b4ac6
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-06 10:55:12 +0200

    script to run with args added

--- include/pipex_msg.h
+++ include/pipex_msg.h
@@ -6,13 +6,15 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 10:05:54 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/06 10:06:52 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/06 10:52:38 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_MSG_H
 # define PIPEX_MSG_H
 
-#define ERROR_ARGC "./pipex <input> <cmd1> <cm2> <output>\n"
+# define ERROR_ARGC "./pipex <input> <cmd1> <cm2> <output>\n"
+
+# define ERROR_FORK "Error when creating the fork\n"
 
 #endif
--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 09:53:03 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/06 10:47:27 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/06 10:54:54 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,11 @@ int	main(int argc, char **argv, char **envp)
 	// pipe(fds);
 	pid = fork();
 	if (pid == -1)
-	printf("Hola\n");
+		perror(ERROR_FORK);
+	else if (pid == 0)
+		printf("Parent\n");
+	else
+		printf("Child with pid %d\n", pid);
+	waitpid(pid, NULL, 0);
 	return (0);
 }
