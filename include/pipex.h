116e5cb9f843cde850f8cb0199d138caf0f8a249
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-19 09:50:58 +0200

    pipe ready at child proccess to get the file and start executing the command

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 09:57:44 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/19 09:35:03 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/19 09:48:31 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,14 @@
 
 # include "tools.h"
 
-# define FD_INPUT 0
-# define FD_OUTPUT 4
-
 # define STDIN 0
+# define STDOUT 1
+# define STDERROR 2
+
+# define F_INPUT 0
+# define F_OUTPUT 3
+
+# define PIPE_READ 0
+# define PIPE_WRITE 1
 
 #endif
--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 09:53:03 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/19 09:12:42 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/19 09:48:33 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,15 @@ static void	child_proccess(int fd[2], char **argv, char **env)
 {
 	int	fd_file;
 
-	fd_file = open(argv[FD_INPUT], O_RDWR);
+	close(fd[PIPE_READ]);
+	fd_file = open(argv[F_INPUT], O_RDWR);
 	if (fd_file == -1)
-		end_error_file(argv[FD_INPUT]);
+		end_error_file(argv[F_INPUT]);
+	dup2(fd_file, STDIN);
+	close(fd_file);
+	dup2(fd[PIPE_WRITE], STDOUT);
+	close(fd[PIPE_WRITE]);
+	
 }
 
 int	main(int argc, char **argv, char **envp)
