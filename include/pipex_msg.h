b2a5655da9a41d8a7d552ea1dab4b8b7f9b11a10
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-19 08:53:51 +0200

    pipe now created with error management and msgs updated

--- include/pipex_msg.h
+++ include/pipex_msg.h
@@ -6,15 +6,16 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 10:05:54 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/06 10:52:38 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/19 08:53:29 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_MSG_H
 # define PIPEX_MSG_H
 
-# define ERROR_ARGC "./pipex <input> <cmd1> <cm2> <output>\n"
+# define ERROR_ARGC "Invalid arguments:\n./pipex <input> <cmd1> <cm2> <output>\n"
 
-# define ERROR_FORK "Error when creating the fork\n"
+# define ERROR_FORK "Error when creating the fork.\n"
+# define ERROR_PIPE "Error when creating the pipe.\n"
 
 #endif
--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 09:53:03 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/19 08:51:20 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/19 08:52:11 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 int	main(int argc, char **argv, char **envp)
 {
 	int	pid;
-	// int	fds[2];
+	int	fds[2];
 
 	if (argc != 5)
 		end(1, ERROR_ARGC);
-	// pipe(fds);
+	if (pipe(fds) == -1)
+		end(1, ERROR_PIPE);
 	pid = fork();
 	if (pid == -1)
 		perror(ERROR_FORK);
