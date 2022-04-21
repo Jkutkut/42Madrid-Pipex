14f14f0e8d0419731377620b3aa0423bb77490e3
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-21 09:35:47 +0200

    working on struct to keep code organized

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 09:57:44 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/21 09:25:22 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/21 09:35:27 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@
 # include <errno.h>
 # include <sys/stat.h>
 
-# include "pipex_msg.h"
-
 # include "libft.h"
 
+# include "pipex_msg.h"
+# include "pipex_structs.h"
 # include "tools.h"
 
 # define STDIN 0
