b718879489060df831e6d7a5dbc7f0d93c6c25d4
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-23 22:15:18 +0200

    end refactored with perror

--- src/tools/end.c
+++ src/tools/end.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 10:44:21 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/21 09:18:13 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/23 22:14:59 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@
  * @brief Ends the execution of the program
  * 
  * @param endtype Type of end (0: normal, 1: error)
- * @param msg Message to display 
+ * @param msg Message to display if error.
  */
 void	end(int endtype, char *msg)
 {
 	if (endtype == 1)
-		ft_putstr_fd("Error:\n", 2);
-	ft_putstr_fd(msg, endtype + 1);
+	{
+		perror("Error:\n  ");
+		perror(msg);
+	}
 	exit(endtype);
 }
