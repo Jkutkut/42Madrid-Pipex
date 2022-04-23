b440bdb9dbbec053635bf2e4ae1748200b587287
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-23 23:54:38 +0200

    free_array refactored to be able to free all kinds of arrays

--- src/tools/free_end.c
+++ src/tools/free_end.c
@@ -6,21 +6,21 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 22:15:32 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/23 23:22:00 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/23 23:57:21 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tools.h"
 
-static void	free_array(void **array)
+static void	free_array(char **array)
 {
 	int	i;
 
 	if (!array)
 		return ;
 	i = 0;
-	while (array + i)
-		free(array + i++);
+	while (*(array + i))
+		free(*(array + i++));
 	free(array);
 }
 
@@ -33,6 +33,6 @@ void	free_end(t_pipex *p, int endtype, char *msg)
 	if (p->env_paths)
 		free_array(p->env_paths);
 	if (p->fds)
-		free_array(p->fds);
+		free(p->fds);
 	end(endtype, msg);
 }
