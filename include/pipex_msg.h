865b51c635563e4368475dea5091c967028fa695
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-30 22:21:35 +0200

    Msg updated to be more precise.
    here_doc needs to be refactored

--- include/pipex_msg.h
+++ include/pipex_msg.h
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 10:05:54 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/05/30 18:38:31 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/05/30 22:21:28 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 
 # define ERROR_ENV "Error when getting the environment variable.\n"
 # define ERROR_PIPE_INIT "Error when initializing pipes.\n"
-# define ERROR_CNF "Command not found.\n"
+# define ERROR_CNF "Command not found or not allowed to execute.\n"
 # define ERROR_CNF_CODE 127
 
 # define ERROR_EXE_CMD "Error when executing the command.\n"
