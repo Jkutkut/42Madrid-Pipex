28b96f88bde884ccf9cb6e155e80b4a11e3db3f6
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-01 14:26:32 +0200

    documented struct commented and the same added without comments to pass the norm

--- include/pipex_structs.h
+++ include/pipex_structs.h
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 09:33:44 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/23 16:05:32 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/06/01 14:26:21 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,59 +15,78 @@
 
 # include "pipex.h"
 
+// /**
+//  * @struct t_pipex
+//  * @brief Struct to hold all the information of the pipex program.
+//  */
+// typedef struct s_pipex
+// {
+// 	/**
+// 	 * @brief Fd of the input file.
+// 	 */
+// 	int		f_input;
+// 	/**
+// 	 * @brief Fd of the output file.
+// 	 */
+// 	int		f_output;
+// 	/**
+// 	 * @brief Array with the cmds (argv)
+// 	 */
+// 	char	**cmds;
+// 	/**
+// 	 * @brief Array with the path array.
+// 	 * Result of processing envp
+// 	 */
+// 	char	**env_paths;
+// 	/**
+// 	 * @brief Number of cmds to execute.
+// 	 */
+// 	int		cmd_count;
+// 	/**
+// 	 * @brief Index of the current cmd.
+// 	 */
+// 	int		cmd_idx;
+// 	/**
+// 	 * @brief Array to store all the fds of the pipes.
+// 	 * If index is even, it's the read fd, if odd, it's the write fd.
+// 	 */
+// 	int		*fds;
+// 	/**
+// 	 * @brief Command to execute as an array.
+// 	 * Defined at each process.
+// 	 * Result of spliting the string by spaces.
+// 	 */
+// 	char	**cmd_args;
+// 	/**
+// 	 * @brief Command with the path.
+// 	 */
+// 	char	*cmd_full;
+// 	/**
+// 	 * @brief 0 if here_doc is not present, 1 if it is.
+// 	 */
+// 	int		here_doc;
+// 	/**
+// 	 * @brief Process id of the current process.
+// 	 */
+// 	int		pid;
+// }				t_pipex;
+
 /**
  * @struct t_pipex
  * @brief Struct to hold all the information of the pipex program.
  */
 typedef struct s_pipex
 {
-	/**
-	 * @brief Fd of the input file.
-	 */
 	int		f_input;
-	/**
-	 * @brief Fd of the output file.
-	 */
 	int		f_output;
-	/**
-	 * @brief Array with the cmds (argv)
-	 */
 	char	**cmds;
-	/**
-	 * @brief Array with the path array.
-	 * Result of processing envp
-	 */
 	char	**env_paths;
-	/**
-	 * @brief Number of cmds to execute.
-	 */
 	int		cmd_count;
-	/**
-	 * @brief Index of the current cmd.
-	 */
 	int		cmd_idx;
-	/**
-	 * @brief Array to store all the fds of the pipes.
-	 * If index is even, it's the read fd, if odd, it's the write fd.
-	 */
 	int		*fds;
-	/**
-	 * @brief Command to execute as an array.
-	 * Defined at each process.
-	 * Result of spliting the string by spaces.
-	 */
 	char	**cmd_args;
-	/**
-	 * @brief Command with the path.
-	 */
 	char	*cmd_full;
-	/**
-	 * @brief 0 if here_doc is not present, 1 if it is.
-	 */
 	int		here_doc;
-	/**
-	 * @brief Process id of the current process.
-	 */
 	int		pid;
 }				t_pipex;
 
