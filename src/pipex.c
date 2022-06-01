696590a46e35ec156790382009fb7cd4b7eb1ae5
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-01 14:43:09 +0200

    Code with Norminette OK

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 09:53:03 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/05/30 18:38:41 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/06/01 14:40:16 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,31 +23,21 @@ static void	exe_cmd(t_pipex *p)
 		use_pipe(&p->fds[p->cmd_idx * 2 - 2], &p->f_output);
 	else
 		use_pipe(&p->fds[p->cmd_idx * 2 - 2], &p->fds[p->cmd_idx * 2 + 1]);
-	close_pipes(p); // Close now, we have it on stdin and stdout
+	close_pipes(p);
 	p->cmd_args = ft_split(p->cmds[p->cmd_idx], ' ');
 	if (!p->cmd_args)
 		free_end(p, 1, ERROR_MALLOC);
 	p->cmd_full = get_path(p->cmd_args[0], p->env_paths);
 	if (!p->cmd_full)
 		free_end(p, ERROR_CNF_CODE, ERROR_CNF);
-
-	ft_putendl_fd(p->cmd_full, STDERROR); // TODO DEBUG
-	if (execve(p->cmd_full, p->cmd_args, p->env_paths) == -1) // ends execution if successful
+	if (execve(p->cmd_full, p->cmd_args, p->env_paths) == -1)
 		free_end(p, 1, ERROR_EXE_CMD);
 }
 
-static int wexitstatus(int status)
+static int	wexitstatus(int status)
 {
 	return (((status) & 0xff00) >> 8);
 }
-
-/*
-static void	check_this(void)
-{
-	// system("lsof -c pipex");
-	system("ps -lf");
-}
-*/
 
 /**
  * @brief Simulates the pipe command from linux.
@@ -59,15 +49,15 @@ static void	check_this(void)
 int	main(int argc, char **argv, char **envp)
 {
 	t_pipex	pipex;
+	int		result;
+	int		idx;
 
-	// atexit(check_this);
 	if (argc < 5)
 		end(1, ERROR_ARGC);
 	init_pipex(&pipex, argc, argv, envp);
 	while (++pipex.cmd_idx < pipex.cmd_count)
 		exe_cmd(&pipex);
-	int result = 0;
-	int idx = 0;
+	idx = 0;
 	while (++idx < pipex.cmd_count - 1)
 		waitpid(-1, NULL, 0);
 	waitpid(-1, &result, 0);
