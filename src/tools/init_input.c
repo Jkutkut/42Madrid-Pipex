2df8042a342083ad568af69687cfda3e9541d115
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-26 09:02:59 +0200

    heredoc debug ooutput removed

--- src/tools/init_input.c
+++ src/tools/init_input.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 21:38:04 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/06/01 14:34:59 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/06/26 08:58:47 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,12 @@ void	init_input(t_pipex *pipex, char ***argv)
 {
 	if (!ft_strncmp((*argv)[1], "here_doc", 9))
 	{
-		ft_putendl_fd("open heredoc", STDIN);
 		pipex->here_doc = 1;
 		heredoc((*argv)[2], pipex);
 		(*argv)++;
 	}
 	else
 	{
-		ft_putendl_fd("open no heredoc", STDIN);
 		pipex->f_input = open((*argv)[1], O_RDONLY);
 		if (pipex->f_input == -1)
 			end_error_file(0, pipex, *((*argv) + 1));
