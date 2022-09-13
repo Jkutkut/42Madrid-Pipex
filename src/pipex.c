9934eb7b24ed8bfdb29b5eb57fc1da77aa9121df
jkutkut <jorgeregonzalez@gmail.com>
2022-09-13 20:00:13 +0200

    working on new implementation

---
+++ src/pipex.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/13 17:15:28 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/09/13 19:42:45 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+/**
+ * @brief Simulates the pipe command from linux.
+ * 
+ * ./pipex file1 cmd1 ... cmdn file2
+ * 
+ * ./pipex here_doc LIMITER cmd1 ... cmdn file2
+ */
+int	main(int argc, char **argv, char **envp)
+{
+	// t_pipex	pipex;
+	// int		result;
+	// int		idx;
+
+	// if (argc < 5)
+	// 	end(1, ERROR_ARGC);
+	// init_pipex(&pipex, argc, argv, envp);
+	// while (++pipex.cmd_idx < pipex.cmd_count)
+	// 	pipex.pid[pipex.cmd_idx - 1] = exe_cmd(&pipex);
+	// idx = 0;
+	// while (idx++ < pipex.cmd_count - 1)
+	// 	waitpid(pipex.pid[idx], NULL, 0);
+	// waitpid(pipex.pid[idx], &result, 0);
+	// if (wifexited(result))
+	// 	free_end(&pipex, wexitstatus(result), NULL);
+	// free_end(&pipex, EXIT_SUCCESS, NULL);
+	return (EXIT_SUCCESS);
+}
