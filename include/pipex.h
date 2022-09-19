5cbec01266f2ac7b1227b18ee6591cd691c06adb
Jkutkut <jorgeregonzalez@gmail.com>
2022-09-19 15:54:34 +0200

    headers logic simplified

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 09:57:44 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/09/19 15:21:21 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/09/19 15:53:57 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@
 
 # include "pipex_msg.h"
 # include "pipex_structs.h"
-# include "pipex_tools.h"
 
 # define STDIN 0
 # define STDOUT 1
@@ -38,5 +37,28 @@
 
 # define PIPE_READ 0
 # define PIPE_WRITE 1
+
+# define HEREDOC_FILE ".heredoc.tmp"
+# define HEREDOC "heredoc> "
+
+/***************************************/
+
+// Arguments parsing
+void	init_pipex(t_pipex *pipex, int argc, char **argv, char **envp);
+
+// void	close_pipe(int *fd);
+// void	close_pipes(t_pipex *p);
+// void	end_error_file(int type, t_pipex *pipex, char *file);
+// void	end(int endtype, char *msg);
+// void	free_array(char **array);
+// void	free_end(t_pipex *p, int endtype, char *msg);
+// char	**get_path_array(char **envp);
+// char	*get_path(char *cmd_full, char **envp);
+// void	heredoc(char *argv, t_pipex *pipex);
+// void	init_input(t_pipex *pipex, char ***argv);
+// void	init_output(t_pipex *pipex, char *path);
+// void	init_pipes(t_pipex *pipex);
+// char	*make_path(char *path, char *cmd);
+// void	use_pipe(int *fd_in, int *fd_out);
 
 #endif
