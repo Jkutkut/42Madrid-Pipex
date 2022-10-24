5a398362c96ea0cd16b5f9908c9ec1189df9ca06
Jkutkut <jorgeregonzalez@gmail.com>
2022-10-24 11:40:56 +0200

    header files for mandatory added

---
+++ include/mandatory/pipex_msg.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_msg.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/06 10:05:54 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/10/24 11:33:05 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_MSG_H
+# define PIPEX_MSG_H
+
+# define ERROR_ARGC "Invalid arguments:\n./pipex <input> <cmd1> <cm2> <output>\n"
+// # define ERROR_FORK "Error when creating the fork.\n"
+// # define ERROR_PIPE "Error when creating the pipe.\n"
+// # define ERROR_MALLOC "Malloc failed.\n"
+// # define ERROR_ENV "Error when getting the environment variable.\n"
+// # define ERROR_PIPE_INIT "Error when initializing pipes.\n"
+// # define ERROR_CNF "Command not found or not allowed to execute.\n"
+// # define ERROR_CNF_CODE 127
+// # define ERROR_EXE_CMD "Error when executing the command.\n"
+
+#endif
---
+++ include/mandatory/pipex_structs.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_structs.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/21 09:33:44 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/10/24 11:26:01 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_STRUCTS_H
+# define PIPEX_STRUCTS_H
+
+# include "pipex_bonus.h"
+
+/**
+ * @struct t_pipex
+ * @brief Struct to hold all the information of the pipex program.
+ */
+typedef struct s_pipex
+{
+	int		f_input;
+	int		f_output;
+	char	*cmds[2];
+	char	**envp;
+	char	**env_paths;
+	int		cmd_idx;
+	int		fds[2];
+	char	**cmd_args;
+	char	*cmd_full;
+	int		*pid;
+}				t_pipex;
+#endif
