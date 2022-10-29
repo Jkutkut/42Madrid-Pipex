2e81f45d2234423bd9d33efa1b92b09260947ddd
Jkutkut <jorgeregonzalez@gmail.com>
2022-10-29 22:08:00 +0200

    tests passed

--- .tests/remove_testers.sh
+++ .tests/remove_testers.sh
@@ -7,7 +7,7 @@ main() {
 		return
 	fi
 
-	repos="https://github.com/vfurmane/pipex-tester.git https://github.com/ael-bekk/PIPEX_TESTER.git https://github.com/Yoo0lh/pipex_tester_42.git https://github.com/gmarcha/pipexMedic.git https://github.com/mateusmedeir/pipexTester.git"
+	repos="https://github.com/vfurmane/pipex-tester.git https://github.com/Yoo0lh/pipex_tester_42.git"
 
 	for r in $repos; do
 		repo=$(echo $r | cut -d'/' -f5 | cut -d'.' -f1)
